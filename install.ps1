92eca9b324565a8a8cbd2e0d3503f7da67109a2f
Matts123 <164667048+3eJleHKa123@users.noreply.github.com>
2026-05-26 04:30:16 +0300

    Delete install.ps1

--- install.ps1
+++
@@ -1,6 +0,0 @@
-$folder="$env:LOCALAPPDATA\SysAdminCheat"
-New-Item -ItemType Directory -Force -Path $folder | Out-Null
-$url="https://github.com/USERNAME/dem/releases/latest/download/SysAdminCheat.exe"
-$out="$folder\SysAdminCheat.exe"
-Invoke-WebRequest -Uri $url -OutFile $out
-Start-Process $out
